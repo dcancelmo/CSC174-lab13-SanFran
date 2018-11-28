96c64cceb5bc05f76ae0003152b3694beb583fb8
jmedina7 <43220330+jmedina7@users.noreply.github.com>
2018-11-28 15:50:22 -0500

    Added a slider to the survey

--- survey.php
+++ survey.php
@@ -26,6 +26,17 @@
         <input type="text" name="cupcake" id="cupcake">
         <label class="fixed" for="frosting">Favorite frosting: </label>
         <input type="text" name="frosting" id="frosting">
+        <input type="range" min="1" max="60" value="1" class="slider" id="budget">
+        <p>How much do you spend per week on cupcakes?: $<span id="budget"></span></p>
+        <script>
+          var slider = document.getElementById("budgetSlider");
+          var output = document.getElementById("budgetValue");
+          output.innerHTML = slider.value;
+
+          slider.oninput = function() {
+          output.innerHTML = this.value;
+          }
+        </script>
         <fieldset>
         <h3><label for="userrecipes">Submit your own recipe below!</label></h3>
         <textarea name="userrecipes" id="userrecipes" cols ="60" rows="30"></textarea>
