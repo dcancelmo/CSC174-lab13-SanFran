5fc866bd5672c241c0a159308b81997f87d85d69
jmedina7 <43220330+jmedina7@users.noreply.github.com>
2018-11-30 04:21:37 -0500

    added images to about

--- about.php
+++ about.php
@@ -26,6 +26,8 @@
 	<main>
 		<h1>About Cupcakes</h1>
 
+		<figure><img src = images/history.jpg></figure>
+
 		<section>
 			<h2>Cupcake History</h2>
 			<p>The earliest extant description of what is now often called a cupcake was in 1796, when a recipe for "a light cake to bake in small cups" was written in American Cookery by Amelia Simmons. The earliest extant documentation of the term cupcake itself was in "Seventy-five Receipts for Pastry, Cakes, and Sweetmeats" in 1828 in Eliza Leslie's Receipts cookbook.</p>
@@ -34,6 +36,9 @@
 
 		<section>
 			<h2>Making Cupcakes</h2>
+
+			<figure><img src = images/tin.jpg></figure>
+
 			<p>A standard cupcake uses the same basic ingredients as standard-sized cakes: butter, sugar, eggs, and flour. Nearly any recipe that is suitable for a layer cake can be used to bake cupcakes. The cake batter used for cupcakes may be flavored or have other ingredients stirred in, such as raisins, berries, nuts, or chocolate chips.</p>
 			<p>Because their small size is more efficient for heat conduction, cupcakes bake much faster than a normal layered cake. During baking, the volume of the batter initially increases due to the production of carbon dioxide, then decreases upon cooling due to the release of leavening gases.</p>
 			<p>Cupcakes may be topped with frosting or other cake decorations. They may be filled with frosting, fruit, or pastry cream. For bakers making a small number of filled cupcakes, this is usually accomplished by using a spoon or knife to scoop a small hole in the top of the cupcake. Another method is to just insert the pastry bag in the middle of the cupcake. In commercial bakeries, the filling may be injected using a syringe. Elaborately decorated cupcakes may be made for special occasions.</p>
@@ -41,6 +46,9 @@
 
 		<section>
 			<h2>Cupcake Variants</h2>
+
+			<figure><img src = images/variants.jpg></figure>
+
 			<p>A cake in a mug is a variant that gained popularity on many internet cooking forums and mailing lists. The technique uses a mug as its cooking vessel and can be done in a microwave oven. The recipe often takes fewer than five minutes to prepare. This variant has become popularised in recent years by the presence of numerous videos on social media websites, each claiming to detail the fastest method to create the finished product.</p>
 			<p>A butterfly cake is a variant of cupcake, also called fairy cake for its fairy-like "wings". The top of the fairy cake is cut off or carved out, and cut in half. Then, butter cream, whipped cream or other sweet filling is spread into the hole. Finally, the two cut halves are stuck into the butter cream to resemble butterfly wings. The wings of the cake are often decorated using icing to form various patterns.</p>
 			<p>A gourmet cupcake is a somewhat recent variant of cupcake. Gourmet cupcakes are large and filled cupcakes, based around a variety of flavor themes, such as Tiramisu or Cappuccino. In recent years there has been an upcropping of stores that sell only gourmet cupcakes in metropolitan areas. As an alternative to a plate of individual cakes, some bakers place standard cupcakes into a pattern and frost them to create a large design, such as a basket of flowers or a turtle.</p>
