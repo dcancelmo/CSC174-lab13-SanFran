3f48b9d6b411f5ead42f6ecff00c62c22aad06f5
famousclark <clark.famous@gmail.com>
2018-12-10 14:57:06 -0500

    footer fix

--- index.php
+++ index.php
@@ -34,21 +34,35 @@
 	</main>
 
 	<footer class='page-footer' >
-		<section class='container'>
-			<h3>Cupcakes? What are those?</h3>
-			<div class="divider"></div>
-			<p>A cupcake is a small cake designed to serve one person, which may be baked in a small thin paper or aluminum cup. As with
-				larger cakes, icing and other cake decorations such as fruit and candy may be applied. Come <a href=about.php>learn all about cupcakes</a>, <a href=recipes.php>read some amazing cupcake recipes</a>, and fill out our <strong><a href=survey.php>CUPCAKE
-						SURVEY!</a></strong></p>
-		</section>
-		<section class='container section'>
-			<a href=login-files/login.php>Admin Login</a>
-		</section>
-		<section class='footer-copyright section'>
-			<div class="container">
-				<a href=survey.php>Fill out our CUPCAKE SURVEY and have your cupcake recipe featured!</a>
+		<div class="row">
+			<div class="col s12 m6">
+				<section class='container'>
+					<h3>Cupcakes? What are those?</h3>
+					<div class="divider"></div>
+					<p>A cupcake is a small cake designed to serve one person, which may be baked in a small thin paper or aluminum cup. As with larger cakes, icing and other cake decorations such as fruit and candy may be applied. Come <a href=about.php>learn all about cupcakes</a>, <a href=recipes.php>read some amazing cupcake recipes</a>, and fill out our
+					<strong>
+						<a href=survey.php>CUPCAKE
+							SURVEY!
+						</a>
+					</strong>
+				</p>
+				</section>
 			</div>
-		</section>
+
+			<div class="col s12 m6">
+				<section class='container'>
+					<h3>Admin?</h3>
+					<div class="divider"></div>
+					<a href=login-files/login.php>Admin Login</a>
+				</section>
+
+				<section class='container'>
+					<h3>Take a survery</h3>
+					<div class="divider"></div>
+					<a href=survey.php>Fill out our CUPCAKE SURVEY and have your cupcake recipe featured!</a>
+				</section>
+			</div>
+		</div>
 	</footer>
 </body>
 
