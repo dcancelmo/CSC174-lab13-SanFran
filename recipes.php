e1df49100aa32dadcc1df912769427462f4bc8bb
famousclark <clark.famous@gmail.com>
2018-12-10 14:40:44 -0500

    h3 fixes

--- recipes.php
+++ recipes.php
@@ -21,122 +21,153 @@
 		<div class="divider"></div>
 
 		<section>
-
-			<h2>Mug Cupcake</h2>
 			<div class="row">
-		    <div class="col s12 m6">
-					<div class="card">
-						<figure class="card-image">
-							<img src = images/mugcake.jpg alt='Mug Cupcake'>
-						</figure>
-						<div class="card-content">
-			        Pose for your mug shot!
-			      </div>
+				<div class="col s12 m3">
+					<h3>Mug Cupcake</h3>
+				</div>
+
+				<div class="col s12 m6">
+					<h3>Ingredients</h3>
+					<ul>
+						<li>1/4 cup all-purpose flour</li>
+						<li>1/4 cup white sugar</li>
+						<li>2 tablespoons unsweetened cocoa powder</li>
+						<li>1/8 teaspoon baking soda</li>
+						<li>1/8 teaspoon salt</li>
+		 				<li>3 tablespoons milk</li>
+						<li>2 tablespoons canola oil</li>
+						<li>1 tablespoon water</li>
+						<li>1/4 teaspoon vanilla extract</li>
+					</ul>
+
+					<h3>Directions</h3>
+					<ol>
+						<li><p>Mix flour, sugar, cocoa powder, baking soda, and salt in a large microwave-safe mug; stir in milk, canola oil, water, and vanilla extract.</p></li>
+						<li><p>Cook in microwave until cake is done in the middle, about 1 minute 45 seconds.</p></li>
+					</ol>
+					<a href = https://www.allrecipes.com/recipe/241038/microwave-chocolate-mug-cake>Source</a>
+				</div>
+
+				<div class="col s12 m3">
+					<div class="row">
+				    <div class="col s12 m12">
+							<div class="card">
+								<figure class="card-image">
+									<img src = images/mugcake.jpg alt='Mug Cupcake'>
+								</figure>
+								<div class="card-content">
+					        Pose for your mug shot!
+					      </div>
+							</div>
+						</div>
 					</div>
 				</div>
 			</div>
 
-			<h3>Ingredients</h3>
-			<ul>
-				<li>1/4 cup all-purpose flour</li>
-				<li>1/4 cup white sugar</li>
-				<li>2 tablespoons unsweetened cocoa powder</li>
-				<li>1/8 teaspoon baking soda</li>
-				<li>1/8 teaspoon salt</li>
- 				<li>3 tablespoons milk</li>
-				<li>2 tablespoons canola oil</li>
-				<li>1 tablespoon water</li>
-				<li>1/4 teaspoon vanilla extract</li>
-			</ul>
 
-			<h3>Directions</h3>
-			<ol>
-				<li><p>Mix flour, sugar, cocoa powder, baking soda, and salt in a large microwave-safe mug; stir in milk, canola oil, water, and vanilla extract.</p></li>
-				<li><p>Cook in microwave until cake is done in the middle, about 1 minute 45 seconds.</p></li>
-			</ol>
-			<a href = https://www.allrecipes.com/recipe/241038/microwave-chocolate-mug-cake>Source</a>
+
 		</section>
 
 		<section>
-			<h2>Butterfly Cake</h2>
+
 			<div class="row">
-		    <div class="col s12 m6">
-					<div class="card">
-						<figure
-							class="card-image">
-							<img src = images/butterfly.jpeg>
-						</figure>
-						<div class="card-content">
-			        Exxxtraaa butter
-			      </div>
+				<div class="col s12 m3">
+					<h3>Butterfly Cake</h3>
+				</div>
+
+				<div class="col s12 m6">
+					<h3>Ingredients</h3>
+					<ul>
+						<li>2 eggs</li>
+						<li>1 cup self-raising flour</li>
+						<li>1/2 cup butter/margarine</li>
+						<li>1/2 cup Caster sugar</li>
+						<li>2 teaspoons baking powder</li>
+						<li>2 cups and 3 tablespoons icing sugar</li>
+						<li>1/2 cup butter</li>
+						<li>Milk</li>
+					</ul>
+
+					<h3>Directions</h3>
+					<ol>
+						<li><p>Preheat oven to 392° for an eletric Oven and lay out 12 cupcake wrappers in a cupcake tin.</p></li>
+						<li><p>Mix the sugar, flour and baking powder together. Add the butter/margarine and eggs and whisk all of it together.</p></li>
+						<li><p>Using a teaspoon, fill the wrapppers with the mixture. Put the tray in the oven for around 15 minutes or until they are golden brown.</p></li>
+						<li><p>Make the icing by whisking the butter and half the sugar together, once they are mixed add the rest of the sugar and a dash of milk, continue whisking.</p></li>
+						<li><p>Use a knife to cut a upside-down, shallow cone shape out the top of the cake, cut this in half. Fill the hole in the cake with icing and then place the cut out cake on top (like wings.) Sprinkle some icing sugar on top of your cakes.</p></li>
+					</ol>
+					<a href =https://www.bbcgoodfood.com/recipes/1580660/simple-butterfly-cakes>Source</a>
+				</div>
+
+				<div class="col s12 m3">
+					<div class="row">
+						<div class="col s12 m12">
+							<div class="card">
+								<figure
+									class="card-image">
+									<img src = images/butterfly.jpeg>
+								</figure>
+								<div class="card-content">
+									Exxxtraaa butter
+								</div>
+							</div>
+						</div>
 					</div>
 				</div>
 			</div>
 
-			<h3>Ingredients</h3>
-			<ul>
-				<li>2 eggs</li>
-				<li>1 cup self-raising flour</li>
-				<li>1/2 cup butter/margarine</li>
-				<li>1/2 cup Caster sugar</li>
-				<li>2 teaspoons baking powder</li>
-				<li>2 cups and 3 tablespoons icing sugar</li>
-				<li>1/2 cup butter</li>
-				<li>Milk</li>
-			</ul>
-
-			<h3>Directions</h3>
-			<ol>
-				<li><p>Preheat oven to 392° for an eletric Oven and lay out 12 cupcake wrappers in a cupcake tin.</p></li>
-				<li><p>Mix the sugar, flour and baking powder together. Add the butter/margarine and eggs and whisk all of it together.</p></li>
-				<li><p>Using a teaspoon, fill the wrapppers with the mixture. Put the tray in the oven for around 15 minutes or until they are golden brown.</p></li>
-				<li><p>Make the icing by whisking the butter and half the sugar together, once they are mixed add the rest of the sugar and a dash of milk, continue whisking.</p></li>
-				<li><p>Use a knife to cut a upside-down, shallow cone shape out the top of the cake, cut this in half. Fill the hole in the cake with icing and then place the cut out cake on top (like wings.) Sprinkle some icing sugar on top of your cakes.</p></li>
-			</ol>
-			<a href =https://www.bbcgoodfood.com/recipes/1580660/simple-butterfly-cakes>Source</a>
 		</section>
 
 		<section>
-			<h2>Cappuccino Gourmet Cupcake</h2>
 			<div class="row">
-		    <div class="col s12 m6">
-					<div class="card">
-						<figure class="card-image">
-							<img src = images/capcake.jpg>
-						</figure>
-						<div class="card-content">
-			        Now thats a good ass coffe cake
-			      </div>
+				<div class="col s12 m3">
+					<h3>Cappuccino Gourmet Cupcake</h3>
+				</div>
+
+				<div class="col s12 m6">
+					<h3>Ingredients</h3>
+					<ul>
+						<li>1/2 cup butter, softened</li>
+						<li>1/2 (8-oz.) package cream cheese, softened</li>
+						<li>2 cups sugar</li>
+						<li>4 large eggs</li>
+						<li>1 teaspoon vanilla extract</li>
+						<li>3 cups all-purpose flour</li>
+						<li>1 teaspoon baking powder</li>
+						<li>1/2 teaspoon baking soda</li>
+						<li>1/2 teaspoon salt</li>
+						<li>1 (8-oz.) container sour cream</li>
+						<li>Double Shot Latte Buttercream</li>
+						<li>Garnishes: instant espresso, ground cinnamon, and chocolate-covered coffee beans</li>
+					</ul>
+
+					<h3>Directions</h3>
+					<ol>
+						<li><p>Preheat oven to 350°. Beat butter and cream cheese at medium speed with an electric mixer until creamy. Beat in sugar until light and fluffy. Add eggs, 1 at a time, beating until blended after each addition. Stir in vanilla.</p></li>
+						<li><p>Combine flour and next 3 ingredients. Gradually add to butter mixture alternately with sour cream, beating until blended. Spoon batter by rounded tablespoonfuls into lightly greased miniature cupcake tins.</p></li>
+						<li><p>Bake at 350° for 13 to 15 minutes or until a wooden pick inserted in centers comes out clean. Cool in tins on wire racks 5 minutes. Remove from tins to wire racks, and cool completely (about 30 minutes). Pipe or spread Double Shot Latte Buttercream onto cupcakes. Garnish, if desired.</p></li>
+					</ol>
+
+					<p>Note: To prepare regular-size cupcakes, spoon batter into 2 lightly greased 12-cup cupcake tins, filling two-thirds full. Bake at 350° for 22 to 24 minutes or until a wooden pick inserted in centers comes out clean. Cool and decorate as desired. Makes 2 dozen.</p>
+					<a href =https://www.myrecipes.com/recipe/cappuccino-cupcakes>Source</a>
+				</div>
+
+				<div class="col s12 m3">
+					<div class="row">
+						<div class="col s12 m12">
+							<div class="card">
+								<figure class="card-image">
+									<img src = images/capcake.jpg>
+								</figure>
+								<div class="card-content">
+									Now thats a good ass coffe cake
+								</div>
+							</div>
+						</div>
 					</div>
 				</div>
 			</div>
 
-
-			<h3>Ingredients</h3>
-			<ul>
-				<li>1/2 cup butter, softened</li>
-				<li>1/2 (8-oz.) package cream cheese, softened</li>
-				<li>2 cups sugar</li>
-				<li>4 large eggs</li>
-				<li>1 teaspoon vanilla extract</li>
-				<li>3 cups all-purpose flour</li>
-				<li>1 teaspoon baking powder</li>
-				<li>1/2 teaspoon baking soda</li>
-				<li>1/2 teaspoon salt</li>
-				<li>1 (8-oz.) container sour cream</li>
-				<li>Double Shot Latte Buttercream</li>
-				<li>Garnishes: instant espresso, ground cinnamon, and chocolate-covered coffee beans</li>
-			</ul>
-
-			<h3>Directions</h3>
-			<ol>
-				<li><p>Preheat oven to 350°. Beat butter and cream cheese at medium speed with an electric mixer until creamy. Beat in sugar until light and fluffy. Add eggs, 1 at a time, beating until blended after each addition. Stir in vanilla.</p></li>
-				<li><p>Combine flour and next 3 ingredients. Gradually add to butter mixture alternately with sour cream, beating until blended. Spoon batter by rounded tablespoonfuls into lightly greased miniature cupcake tins.</p></li>
-				<li><p>Bake at 350° for 13 to 15 minutes or until a wooden pick inserted in centers comes out clean. Cool in tins on wire racks 5 minutes. Remove from tins to wire racks, and cool completely (about 30 minutes). Pipe or spread Double Shot Latte Buttercream onto cupcakes. Garnish, if desired.</p></li>
-			</ol>
-
-			<p>Note: To prepare regular-size cupcakes, spoon batter into 2 lightly greased 12-cup cupcake tins, filling two-thirds full. Bake at 350° for 22 to 24 minutes or until a wooden pick inserted in centers comes out clean. Cool and decorate as desired. Makes 2 dozen.</p>
-			<a href =https://www.myrecipes.com/recipe/cappuccino-cupcakes>Source</a>
 		</section>
 	</main>
 
